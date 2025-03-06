0eb0236a7c602649ebf42aeb527c107093361007
Rashid Rasul <rstrategist@users.noreply.github.com>
2025-03-06 14:49:03 +0000

    feat: Add file search CLI functionality with tests

---
+++ gcli/gcli.py
@@ -0,0 +1,31 @@
+
+# python3 gcli.py --help
+# python3 gcli.py
+# python3 gcli.py --path . --ftype py
+
+import click
+import glob
+
+# this is bad code intentionally
+# varbad=
+
+
+@click.command()
+@click.option(
+    "--path",
+    prompt="Path to search for files",
+    help="This is the path to search for files: /tmp",
+)
+@click.option(
+    "--ftype", prompt="Pass in the type of file", help="Pass in the file type:  i.e csv"
+)
+def search(path, ftype):
+    results = glob.glob(f"{path}/*.{ftype}")
+    click.echo(click.style("Found Matches:", fg="red"))
+    for result in results:
+        click.echo(click.style(f"{result}", bg="blue", fg="white"))
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    search()
